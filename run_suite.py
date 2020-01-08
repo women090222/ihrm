f2e277190284fece36b7e9c6c2ddb033865ca089
zzy <zzy090222@qq.com>
2020-01-08 12:35:47 +0800

    python

--- run_suite.py
+++ run_suite.py
@@ -16,7 +16,8 @@ suite.addTest(unittest.makeSuite(TestIHRMEmp))
 # suite.addTest(unittest.makeSuite(TestIHRMLogin))
 
 # 3.使用HTMLTestRunner执行测试套件,生成测试报告
-report_path = app.BASE_DIR + '/report/ihrm{}.html'.format(time.strftime('%Y%m%d %H%M%S'))
+# report_path = app.BASE_DIR + '/report/ihrm{}.html'.format(time.strftime('%Y%m%d %H%M%S'))
+report_path=app.BASE_DIR+ '/report/ihrm.html'
 with open(report_path, mode='wb') as f:
     # 初始化HTMLRunner
     runner = HTMLTestReportCN(f, verbosity=2, title='IHRM人力资源接口测试', description='V1.0.0')
